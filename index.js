0b96b5bb1789f7c84cdb18eb0381cbc4173964be
Jaydeep Sagalaka <jaydeepsagalaka@gmail.com>
2025-02-11 22:32:19 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const connectDb = require("./config/db.config");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Pragma",
+      "Expires",
+    ],
+    credentials: true,
+  })
+);
+
+connectDb();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
